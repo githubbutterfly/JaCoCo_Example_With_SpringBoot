bb221520b6fd7d5fd631f75a4187738ced922658
Zarana <zarana20994@gmail.com>
2026-01-29 18:10:57 +0530

    Add files via upload

--- src/main/java/com/example/jacoco/JacocoSpringbootApplication.java
+++ src/main/java/com/example/jacoco/JacocoSpringbootApplication.java
@@ -1,1 +1,12 @@
+package com.example.jacoco;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacocoSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JacocoSpringbootApplication.class, args);
+    }
+}
